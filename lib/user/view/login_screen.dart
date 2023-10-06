a2c15f88c071f823c4596846f2100bfc00abe32a
DongWooKim97 <nanaukim@naver.com>
2023-10-06 17:49:58 +0900

    feat: Dio로 Auth API 요청해보기

--- lib/user/view/login_screen.dart
+++ lib/user/view/login_screen.dart
@@ -1,6 +1,10 @@
+import 'dart:convert';
+import 'dart:io';
+
 import 'package:actual/common/component/custom_text_form_field.dart';
 import 'package:actual/common/const/colors.dart';
 import 'package:actual/common/layout/default_layout.dart';
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 
 class LoginScreen extends StatelessWidget {
@@ -8,6 +12,13 @@ class LoginScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final dio = Dio();
+    // localhost: :  Android / IOS
+    const emulatorIp = '10.0.2.2:3000';
+    const simulatorIp = '127.0.0.1:3000';
+
+    final ip = Platform.isAndroid ? emulatorIp : simulatorIp;
+
     return DefaultLayout(
       child: SingleChildScrollView(
         keyboardDismissBehavior: ScrollViewKeyboardDismissBehavior.onDrag,
@@ -37,14 +48,42 @@ class LoginScreen extends StatelessWidget {
                 ),
                 const SizedBox(height: 16.0),
                 ElevatedButton(
-                  onPressed: () {},
+                  onPressed: () async {
+                    // ID : PWD
+                    const rawString = 'test@codefactory.ai:testtest';
+
+                    // 일반 스트링을 Base64로 바꿀 수 있는 한 줄의 코드!
+                    Codec<String, String> stringToBase64 = utf8.fuse(base64);
+                    String token = stringToBase64.encode(rawString);
+
+                    final resp = await dio.post(
+                      'http://$ip/auth/login',
+                      options: Options(
+                        headers: {
+                          'authorization': 'Basic $token',
+                        },
+                      ),
+                    );
+                  },
                   style: ElevatedButton.styleFrom(
                     primary: PRIMARY_COLOR,
                   ),
                   child: Text('로그인'),
                 ),
                 TextButton(
-                  onPressed: () {},
+                  onPressed: () async {
+                    String refreshToken =
+                        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InRlc3RAY29kZWZhY3RvcnkuYWkiLCJzdWIiOiJmNTViMzJkMi00ZDY4LTRjMWUtYTNjYS1kYTlkN2QwZDkyZTUiLCJ0eXBlIjoicmVmcmVzaCIsImlhdCI6MTY5NjU4MTkxNywiZXhwIjoxNjk2NjY4MzE3fQ.Zc7GL8BmNX3xLvZDsXuLD9N8jFwAou402vigQzoGutg';
+
+                    final resp = await dio.post(
+                      'http://$ip/auth/token',
+                      options: Options(
+                        headers: {
+                          'authorization': 'Bearer $refreshToken',
+                        },
+                      ),
+                    );
+                  },
                   style: TextButton.styleFrom(
                     primary: Colors.black,
                   ),
@@ -89,3 +128,17 @@ class _SubTitle extends StatelessWidget {
     );
   }
 }
+
+// 12. 우리가 IOS시뮬레이터를 쓸 때는 , 시뮬레이터와 컴퓨터의 네트워크가 같다.
+//    그러나 안드로이드 에뮬레이터를 쓰시는 경우에는 네트워크가 다르게 설정되어있다.
+
+// 13. 애뮬레이터, 시뮬레이터 적재적소에 쓰기 위해선 Platform.is~~~를 통해 확인할 수 있다.
+
+// 14. utf8은 전세계적으로 가장 많이 사용하고 있는 흔한 코덱
+
+// 15. Codec<String, String> -> 어떤 값을 어떤 값으로 변환할건지 정의하고 있다.
+//    일반 스트링을 Base64로 바꿀 수 있는 한 줄의 코드!
+//    Codec<String, String> stringToBase64 = utf8.fuse(base64);
+
+// 16. stringToBase64라는 변수에다가 우리가 어떻게 인코딩을 할건지 정리를 하고,
+//      정의를 갖고서 String token = stringToBase64.encode(rawString); 를 통해 인코딩한다. rawString값을!@!
