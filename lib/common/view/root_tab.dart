024a228de4d7f5b2f67edeb17bbac7e9b2a2f223
김동우 <nanaukim@naver.com>
2023-10-08 21:30:31 +0900

    feat: TabBarView 설정

--- lib/common/view/root_tab.dart
+++ lib/common/view/root_tab.dart
@@ -9,8 +9,31 @@ class RootTab extends StatefulWidget {
   State<RootTab> createState() => _RootTabState();
 }
 
-class _RootTabState extends State<RootTab> {
+class _RootTabState extends State<RootTab> with SingleTickerProviderStateMixin {
+  late TabController controller; // 나중에 값을 선언할건데, 사용할 땐 선언이 되어있을거야!
   int index = 0;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    // children에 들어가는 리스트의 길이 , vsync - 컨트롤러를 선언하는 현재의 State를 넣어주면 딤
+    // this가 특정 기술을 가지고 있어야함.
+    controller = TabController(length: 4, vsync: this);
+    controller.addListener(tabListener);
+  }
+
+  void tabListener() {
+    setState(() {
+      index = controller.index;
+    });
+  }
+
+  @override
+  void dispose() {
+    controller.removeListener(tabListener);
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -23,10 +46,7 @@ class _RootTabState extends State<RootTab> {
         unselectedFontSize: 10.0,
         type: BottomNavigationBarType.shifting,
         onTap: (int index) {
-          //int index를 써서 클릭된 버튼들의 인덱스값을 확인할 수 있음.
-          setState(() {
-            this.index = index;
-          });
+          controller.animateTo(index); // animateTo -> 움직여라 index로!
         },
         currentIndex: index,
         items: [
@@ -48,9 +68,26 @@ class _RootTabState extends State<RootTab> {
           ),
         ],
       ),
-      child: Center(
-        child: Text('Root Tab'),
+      child: TabBarView(
+        controller: controller,
+        physics: NeverScrollableScrollPhysics(),
+        children: [
+          Center(child: Container(child: Text('홈'))),
+          Center(child: Container(child: Text('음식'))),
+          Center(child: Container(child: Text('주문'))),
+          Center(child: Container(child: Text('프로필'))),
+        ],
       ),
     );
   }
 }
+
+// vsync가 파라미터로 있는 함수 및 위젯을 사용할 땐 State뒤에 무조건 with SingleTickerProviderStateMixin을 붙여서 사용해야함.
+
+// TabController를 현재 initState에서 하고있는데, 바로 선언하지 않고 initState에서 선언하기 때문에 late라는 타입을 사용해도 된다.
+// late는 바로는 선언하지 않지만, 사용할 땐 선언되어있다고 장담하는 건데, 여기서 그냥 ?를 사용해서 쓰면
+// controller를 사용할 때 마다 null-checking을 매번 해줘야해서 불편하다.
+
+// TabBarView를 사용하는 목적은 앱의 하단부 Tabs(홈, 음식, 주문, 프로필)에 맞게끔 View를 설정하기 위함이다.
+
+// 컨트롤러에서 우리가 값이 변경이 될 때 마다 특정 변수를 실행을 해라라고 하면된다.
