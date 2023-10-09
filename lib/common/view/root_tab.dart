8532629f42aeb5a4d7236c26fd8068252982eb53
김동우 <nanaukim@naver.com>
2023-10-09 13:57:56 +0900

    feat: RestaurantCard 작업하기

--- lib/common/view/root_tab.dart
+++ lib/common/view/root_tab.dart
@@ -1,5 +1,6 @@
 import 'package:actual/common/const/colors.dart';
 import 'package:actual/common/layout/default_layout.dart';
+import 'package:actual/restaurant/restaurant_screen.dart';
 import 'package:flutter/material.dart';
 
 class RootTab extends StatefulWidget {
@@ -70,9 +71,9 @@ class _RootTabState extends State<RootTab> with SingleTickerProviderStateMixin {
       ),
       child: TabBarView(
         controller: controller,
-        physics: NeverScrollableScrollPhysics(),
+        physics: NeverScrollableScrollPhysics(), // 좌우 스크롤 방지.
         children: [
-          Center(child: Container(child: Text('홈'))),
+          RestaurantScreen(),
           Center(child: Container(child: Text('음식'))),
           Center(child: Container(child: Text('주문'))),
           Center(child: Container(child: Text('프로필'))),
